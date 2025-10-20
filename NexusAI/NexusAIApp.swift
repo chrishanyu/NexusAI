2abfade358a4c20cb83863a5833a701fff5aa11a
Your Name <your.email@example.com>
2025-10-20 14:12:26 -0700

    Initial Commit

---
+++ NexusAI/NexusAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusAIApp.swift
+//  NexusAI
+//
+//  Created by Hanyu Zhu on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NexusAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
